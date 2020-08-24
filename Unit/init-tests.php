4e1314a3afabf139f7121c915ab39f5852934467
Rémy Perona <remy@wp-rocket.me>
2020-08-24 10:50:14 -0400

    Fixes missing bootstrap (PR #6)
    * update export-ignore for LICENSE
    * require this bootstrap in addition to parent one

--- Unit/init-tests.php
+++ Unit/init-tests.php
@@ -6,5 +6,6 @@
 define( 'WPMEDIA_PHPUNIT_ADDON_ROOT_TEST_DIR', __DIR__ );
 
 require_once WPMEDIA_PHPUNIT_ROOT_DIR . 'vendor/wp-media/phpunit/Unit/bootstrap.php';
+require_once WPMEDIA_PHPUNIT_ROOT_DIR . 'vendor/wp-media/phpunit-wp-rocket/Unit/bootstrap.php';
 
 define( 'WPMEDIA_IS_TESTING', true ); // Used by wp-media/{package}.
